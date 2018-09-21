411b417b6f8d910350bf837092d158796311e1dc
40623130 <40623130@gm.nfu.edu.tw>
2018-09-21 10:39:17 +0800

    renew blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = '機械設計工程系 2018 Fall 分組網誌'
+AUTHOR = '40623130-陳鉅忠'
+SITENAME = '機械設計工程系 40623130-陳鉅忠 網誌'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
